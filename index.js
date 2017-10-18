ac230adce5799634e96a0f39a4f3fb9567c3ca07
wsavd <tuve@rootfest.net>
2017-10-19 01:29:03 +0300

    2

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ app.post('/files', function (req, res) {
     })
 });
 
-app.listen(3000,function(){
-    console.log("Working on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, function(){
+    console.log('Server listening on:', port)
 });
